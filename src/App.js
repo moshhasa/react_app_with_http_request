b19c289fca8cb40da949acb2d9f066b8d05c20da
Ernest <ernestduodu@hotmail.co.uk>
2021-09-08 10:56:47 +0100

    refactored promise to use asyns/await

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fetchMovieHandler = () => {
-    fetch("https://swapi.dev/api/films")
-      .then((res) => res.json())
-      .then((data) => {
-        const transformedMovies = data.results.map((movie) => {
-          return {
-            id: movie.episode_id,
-            title: movie.title,
-            openingText: movie.opening_crawl,
-            releaseDate: movie.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  const fetchMovieHandler = async () => {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+    
+    const transformedMovies = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+    
   };
 
   return (
